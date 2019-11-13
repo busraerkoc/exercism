fcce5439f5514412d2bb7dde1ce293943a954717
busraerkoc <bsrerkoc@gmail.com>
2019-11-13 23:40:35 +0300

    Initial commit

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,10 @@
+def latest(scores):
+	return scores[-1]
+
+def personal_best(scores):
+        return max(scores)
+
+def personal_top_three(scores):
+	scores.sort()
+	scores.reverse()
+	return scores[0:3]
