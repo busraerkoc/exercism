d98ad3efae3b4c49b6e94d9900b035a1e6b4d6ee
busraerkoc <bsrerkoc@gmail.com>
2020-01-31 12:56:13 +0300

    Resistor color added.

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,6 @@
+def color_code(color):
+	clr = colors()
+	return clr.index(color)
+def colors():
+	color_list = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+	return color_list
