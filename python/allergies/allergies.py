af29435e0a9ccda9d446c7e0c1179eee40b86f81
busraerkoc <bsrerkoc@gmail.com>
2020-04-03 18:32:56 +0300

    Allergies challenge added.

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,25 @@
+
 class Allergies:
 
     def __init__(self, score):
-        pass
+        self.score = score % 256          	#scores
+        self.allergens = ['eggs','peanuts','shellfish', 'strawberries', 'tomatoes','chocolate', 'pollen' ,'cats']
 
     def allergic_to(self, item):
-        pass
+        return item in self.lst
 
     @property
     def lst(self):
-        pass
+        allergens_list = []
+        scores = [1, 2, 4, 8, 16, 32, 64, 128]
+        for i in range(len(self.allergens), -1, -1):
+            if self.score in scores:
+                allergens_list.append(self.allergens[scores.index(self.score)])
+                self.score -=2**(scores.index(self.score))
+            elif self.score >= 2**i:
+                allergens_list.append(self.allergens[i])
+                self.score -= 2**i
+            else: 
+                continue
+        return allergens_list
+
