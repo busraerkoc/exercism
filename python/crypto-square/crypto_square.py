a4a6a2a567ff1081cda1632d6edace932dbf1493
busraerkoc <bsrerkoc@gmail.com>
2020-09-09 15:45:22 +0300

    gitignore added.

--- python/crypto-square/crypto_square.py
+++ python/crypto-square/crypto_square.py
@@ -1,16 +1,7 @@
-from math import ceil, sqrt
+import math
 def cipher_text(plain_text):
-	normalized = "".join(i.lower() for i in plain_text if i.isalnum())
-	length = len(normalized)
-	c = ceil(sqrt(lenght))
-	r = ceil(lenght/c)
-	last = 0
-	last = []
-	k = 0
-	last = c
-	while last < length+c:
-		list.append(normalized[k:last])
-		k = last
-		last +=c
-	for i in r:
- 		new = list
+    normalized = "".join(i.lower() for i in plain_text if i.isalnum())
+    c = math.ceil(math.sqrt(len(normalized)))   # columns
+    r = len(normalized) / c    # rows
+    new = ''.join([normalized[i::c] for i in range(c)])
+    return new
