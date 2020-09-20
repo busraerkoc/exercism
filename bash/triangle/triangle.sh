0763bd24bd3fb7efcc3fd16bd14dd562beab2b0e
busraerkoc <bsrerkoc@gmail.com>
2020-09-20 23:36:09 +0300

    triangle added.

---
+++ bash/triangle/triangle.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+main() {
+
+	[[ $2 == 0 || $3 == 0 || $4 == 0 ]] && { echo "false" ; exit 0 ;}
+	(( $(bc <<< "$2 + $3 < $4 || $2 + $4 < $3 || $3 + $4 < $2") )) && { echo "false" ; exit 0 ; }
+	if [[ $1 == "equilateral" ]]
+	then
+		[[ $2 == $3 && $2 == $4 && $3 == $4 ]] && echo "true" || echo "false"
+	elif [[ $1 == "isosceles" ]]
+	then
+		[[ $2 == $3 || $2 == $4 || $3 == $4 ]] && echo "true" || echo "false"
+	elif [[ $1 == "scalene" ]]
+	then
+			[[ $2 != $3 && $2 != $4 && $3 != $4 ]] && echo "true" || echo "false"
+	fi
+}
+
+main "$@"
