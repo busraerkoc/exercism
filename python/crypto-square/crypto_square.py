76e27b739583ed09cbcf4769e7f807b1dd37c900
busraerkoc <bsrerkoc@gmail.com>
2020-09-30 23:32:13 +0300

    crypto-square completed.

--- python/crypto-square/crypto_square.py
+++ python/crypto-square/crypto_square.py
@@ -1,7 +1,13 @@
 import math
 def cipher_text(plain_text):
+    if plain_text == "":
+        return ""
+    result = ""
     normalized = "".join(i.lower() for i in plain_text if i.isalnum())
     c = math.ceil(math.sqrt(len(normalized)))   # columns
-    r = len(normalized) / c    # rows
-    new = ''.join([normalized[i::c] for i in range(c)])
-    return new
+    r = math.ceil(len(normalized) / c )  # rows
+    if len(normalized) < 2:
+        result = ''.join([normalized[i::c] for i in range(c)])
+    else:
+        result = ''.join([normalized[i::c]+" " for i in range(c)])
+    return result.rstrip()
