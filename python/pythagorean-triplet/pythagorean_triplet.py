03c3bfe10ca12b73265aa9868579bddf154b5c21
busraerkoc <bsrerkoc@gmail.com>
2020-09-30 20:14:55 +0300

    unnecessary functions removed.

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,19 +1,8 @@
-
-import math
 def triplets_with_sum(number):
-	triplets = []
-	for b in range(number):
-		for a in range(1, b):
-			c = math.sqrt(a**2 + b**2)
-			if (a + b + c) == number:
-					triplets.append([a, b, c])	
-	return triplets
-
-def triplets_in_range(start, end):
-    pass
-
-
-def is_triplet(triplet):
-	a, b, c = triplet 
-	return (a**2 + b**2 == c**2) and a < b < c
-
+    triplets = []
+    for a in range(number//2):
+        for b in range(a, number//2):
+            c = number - a - b
+            if (a < b < c) and (a**2 + b**2 == c**2):
+                triplets.append([a, b, c])
+    return triplets
