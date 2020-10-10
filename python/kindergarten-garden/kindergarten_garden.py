894416a9ce8182c04e52a4de139a0a00ee52d26d
busraerkoc <bsrerkoc@gmail.com>
2020-10-10 13:09:51 +0300

    kindergarten-garden added.

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,9 +1,15 @@
+STUDENTS = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
 class Garden:
-	def __init__(self, diagram, students):
-		self.row1 = []
-		self.row2 = []
-		self.students = students
-	
+    def __init__(self, diagram, students=STUDENTS):
+        self.students = students
+        self.diagram = diagram.split() 
+        self.students.sort()
 
-
-
+    def plants(self, student):
+        Plant = {"R" : "Radishes", "C" : "Clover", "G" : "Grass", "V" : "Violets"}
+        result = []
+        indice = self.students.index(student)
+        for i in range(2):
+            result.append(Plant[self.diagram[i][indice*2]])
+            result.append(Plant[self.diagram[i][indice*2+1]])
+        return result
