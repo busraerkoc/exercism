8087966fce7874186de084b5da2973a1aa29385b
busraerkoc <bsrerkoc@gmail.com>
2020-10-11 22:28:13 +0300

    queen-attack added.

--- python/queen-attack/queen_attack.py
+++ python/queen-attack/queen_attack.py
@@ -1,8 +1,13 @@
 class Queen:
 	def __init__(self, row, column):
-        	self.row = row
-                self.column = column
-
+            self.row = row
+            self.column = column
+            if self.row < 0 or self.column < 0 or self.row > 7 or self.column >7: 
+                raise ValueError("Error")
 	def can_attack(self, another_queen):
-        	if self.row == another_queen.row and self.column == another_queen.row 
-		
+            if self.row == another_queen.row and self.column == another_queen.column:
+                raise ValueError("Error")
+            if self.row == another_queen.row or self.column == another_queen.column or abs(self.column-another_queen.column) == abs(self.row-another_queen.row):
+                return True
+            else:
+                return False
